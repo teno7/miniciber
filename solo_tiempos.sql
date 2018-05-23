4e0258b5ca90d85c45bc2600ffaa3e064fe02142
Eufracio Tenoch <tenochsr@gmail.com>
2018-05-23 04:28:40 -0500

    Agregado script de configuracion faltante

---
+++ solo_tiempos.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE Computadoras (
+    nombre VARCHAR(15) UNIQUE PRIMARY KEY, 
+    mac VARCHAR(17) UNIQUE, 
+    ip VARCHAR(15) UNIQUE
+); /* !Fin */
+
+CREATE TABLE Alquiler(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre VARCHAR(15), 
+    inicio REAL,
+    fin REAL, 
+    limite REAL,
+    muerto REAL,
+    abono REAL
+); /* !Fin */
+
+CREATE TABLE Tarifa(
+    minutos INTEGER PRIMARY KEY,
+    cobro REAL
+); /* !Fin */
